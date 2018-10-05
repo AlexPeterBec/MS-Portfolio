41c7f2797218feb3d71aeb729b410550f9f906a9
Alex <alx.bec@me.com>
2018-10-05 15:10:40 +0200

    Add Java MapReduce Project

---
+++ Java Distributed Map-Reduce/src/partFive/Slave.java
@@ -0,0 +1,10 @@
+package partFive;
+
+// https://github.com/macagua/example.java.helloworld
+
+public class Slave {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println(3+5);
+    }
+}
