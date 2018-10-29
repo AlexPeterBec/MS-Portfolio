e96628d6d9b6cab022399cf6b76c6a33111574d2
Alex <alx.bec@me.com>
2018-10-29 15:00:49 +0100

    SQL TP3 and Akka Actor model

--- Java Distributed Map-Reduce/src/partFive/Master.java
+++
@@ -1,9 +0,0 @@
-package partFive;
-
-public class Master {
-
-    public void main(String[] args){
-        ProcessBuilder pb = new ProcessBuilder("ls", "-al", "/tmp");
-        Process p = pb.start();
-    }
-}
